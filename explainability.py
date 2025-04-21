84eadd37a9a673b4d0cb035f39e4f21ecf49c19f
Matheo Angelo <47525004+matheo-angelo@users.noreply.github.com>
2025-04-20 23:05:23 -0300

    Update explainability.py
    Separated fidelity into fidelity+ and fidelity-

--- explainability.py
+++ explainability.py
@@ -9,7 +9,7 @@ def instance_sparsity(m, M):
     
     return np.log(m/M) / np.log(1/M)
 
-def instance_fidelity(model, data, mask, outputs):
+def instance_fidelity_plus(model, data, mask, outputs):
     
     # Calculates Fidelity+_prob, as exemplified in the GNN XAI taxonomic survey paper
     
@@ -19,12 +19,26 @@ def instance_fidelity(model, data, mask, outputs):
     masked_model_prob = outputs[0].max(1)[0]
     return model_prob - masked_model_prob	
 
+def instance_fidelity_minus(model, data, mask, outputs):
+    
+    # Calculates Fidelity-_prob, as exemplified in the GNN XAI taxonomic survey paper
+    
+    model_prob = outputs[0].max(1)[0]
+    old_data_x = data.x.clone()
+    data.x.zero_()
+    data.x[mask] = old_data_x[mask]
+    
+    outputs= model(data.x, data.edge_index, data.batch, data.edge_attr,data.pos)
+    masked_model_prob = outputs[0].max(1)[0]
+    return 1 - (model_prob - masked_model_prob)	
+
 def explain_model(model, explain_loader, sample_indices, n_roi, biomarker_size=10):
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     explanation_list = []
     sparsity_sum = 0
-    fidelity_sum = 0
+    fidelity_plus_sum = 0
+    fidelity_minus_sum = 0
     mask_size = None
     max_entropy_distribution = torch.ones(n_roi)/n_roi
     mask_distribution = torch.zeros(n_roi, device=device)
@@ -46,16 +60,20 @@ def explain_model(model, explain_loader, sample_indices, n_roi, biomarker_size=1
         
         mask_distribution[node_mask] += 1
 
-          #Calculate fidelity
-        fidelity = instance_fidelity(model, data, node_mask, outputs)
-        fidelity_sum += fidelity
+        # Calculate fidelity
+        fidelity_plus = instance_fidelity_plus(model, data, node_mask, outputs)
+        fidelity_plus_sum += fidelity_plus
+
+        fidelity_minus = instance_fidelity_minus(model, data, node_mask, outputs)
+        fidelity_minus_sum += fidelity_minus
 
     sparsity = sparsity_sum	/ len(explain_loader)
-    fidelity = (fidelity_sum	/ len(explain_loader)).item()
+    fidelity_plus = (fidelity_plus_sum	/ len(explain_loader)).item()
+    fidelity_minus = (fidelity_minus_sum	/ len(explain_loader)).item()
     mask_entropy = Categorical(probs = mask_distribution / mask_distribution.sum()).entropy()
     max_possible_entropy = Categorical(probs = max_entropy_distribution).entropy()
     normalized_mask_entropy = mask_entropy / max_possible_entropy
     biomarker = (torch.topk(mask_distribution, biomarker_size, sorted=True)[1]).tolist()
 
-    return explanation_list, fidelity, sparsity, normalized_mask_entropy, biomarker
+    return explanation_list, fidelity_plus, fidelity_minus, sparsity, normalized_mask_entropy, biomarker
 
